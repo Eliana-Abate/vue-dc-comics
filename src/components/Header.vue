0e4c7b520170451f85c8db3e5f181a804c352188
Eliana-Abate <eliana.abate@outlook.it>
2021-09-06 16:16:49 +0200

    added dinamic compilation of 'li>a' and style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,16 @@
 <template>
-  <header>
+  <header class="row">
+      <div id="header-logo">
+          <img src="@/assets/img/dc-logo.png" alt="dc-logo">
+      </div>
+
+      <div>
+          <nav>
+              <ul>
+                  <li v-for = "(link, index) in links" :key="index"><a :href="link.url">{{link.text}}</a></li>
+              </ul>
+          </nav>
+      </div>
 
   </header>
 </template>
@@ -7,10 +18,71 @@
 <script>
 export default {
     name: "Header",
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ],
+
+        };
+    },
 
 }
 </script>
 
 <style>
+    header {
+        background-color: #fff;
+        height: 120px;
+    }
 
 </style>
